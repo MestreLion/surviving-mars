5aa90604b0919a4395757ce843deb6542e0e345b
MestreLion <linux@rodrigosilva.com>
2024-10-19 04:31:13 -0300

    refactor to improve docs, legibility and clarity

--- sensortower_heatmap_static/main.py
+++ sensortower_heatmap_static/main.py
@@ -19,7 +19,7 @@ import matplotlib.pylab as plt
 import numpy as np
 import seaborn as sns
 
-# From game data
+# Constants from game data
 # https://github.com/surviving-mars/SurvivingMars/blob/master/Lua/_GameConst.lua#L102
 SECTOR_GRID = (10, 10)
 SECTOR_SIZE = (40, 40)
@@ -29,13 +29,14 @@ MAX_RANGE = 120      # range of no boost (boost = 0%)
 NUM_BOOST =  10      # every working tower provides this much cumulative boost to all sectors
 NUM_BOOST_MAX = 100  # max cumulative scan boost
 
-# Derived
-MAP_SIZE = np.multiply(SECTOR_GRID, SECTOR_SIZE)  # tuple(map(operator.mul, SECTOR_GRID, SECTOR_SIZE))  # (400, 400)
+# Derived constants
+MAP_SIZE = np.multiply(SECTOR_GRID, SECTOR_SIZE)  # (400, 400)
 
-# Custom
+# Parameters default values
 TOWERS_GRID_RANK = 3
 
 log = logging.getLogger(os.path.basename(os.path.splitext(__file__)[0]))
+
 
 @dataclasses.dataclass
 class MapSector:
@@ -51,7 +52,8 @@ class MapSector:
     def distance(self, point):
         return math.dist(self.center, point)
 
-    def scan_boost(self, towers):
+    def scan_boost(self, towers) -> int:
+        """Scan boost for this Sector given the towers placement"""
         # https://github.com/surviving-mars/SurvivingMars/blob/master/Lua/Exploration.lua#L284
         # function MapSector:GetTowerBoost(city)
         boost = clamp(len(towers) * NUM_BOOST, NUM_BOOST_MAX)
@@ -60,33 +62,42 @@ class MapSector:
             boost += scale(BOOST_MAX,  clamp(MAX_RANGE - best, self.BOOST_RANGE), self.BOOST_RANGE)
         return boost
 
-def scale(value, numerator, divisor) -> int:
-    return round(value * numerator / divisor)
+    @classmethod
+    def map_scan_boost(cls, towers) -> np.ndarray:
+        """Scan boost for all sectors in given the towers placement"""
+        def sector_scan_boost(sx, sy):
+            return cls(sx, sy).scan_boost(towers)
+        return np.fromfunction(np.vectorize(sector_scan_boost, otypes=[int]), SECTOR_GRID)
+
+
+def scale(value, numerator, denominator) -> int:
+    """Helper function mimicking Surviving Mars' MulDivRound()"""
+    return round(value * numerator / denominator)
+
 
 def clamp(value, upper=None, lower=None):
+    """Helper function to replace min()/max() usage as upper/lower bounds of a value"""
     v = value
     if upper is not None: v = min(value, upper)
     if lower is not None: v = max(value, lower)
     return v
 
-def inner_grid(grid_size, rank: int):
-    axes = (np.linspace(0, s, num=rank+2)[1:-1] for s in grid_size)
+
+def inner_grid(area_size, rank: int):
+    """
+    Coordinates of a rank x rank grid of points evenly spaced on an outer area
+
+    The grid is also equally spaced away from the area borders, hence "inner" grid
+    Coordinates of a rank 3 grid (9 points) over a (400, 400) area are:
+    [(100, 100), (100, 200), (100, 300),
+     (200, 100), (200, 200), (200, 300),
+     (300, 100), (300, 200), (300, 300)]
+
+    Return a 2-Tuple of the list of 2D coordinates and its corresponding Numpy meshgrid
+    """
+    axes = (np.linspace(0, s, num=rank+2)[1:-1] for s in area_size)
     grid = np.meshgrid(*axes, indexing='ij')
     return np.vstack(list(map(np.ravel, grid))).T, grid
-
-def scan_boost(sx, sy, towers=None) -> int:
-    # 	local best, boost = 0
-    # 	for tower in (city.labels.SensorTower or empty_table)
-    # 		if tower.working then
-    # 			boost = boost + 1
-    # 			best = IsCloser2D(self, tower, best or max_range) and tower or best
-    # 	boost = Min(boost * const.SensorTowerCumulativeScanBoost, const.SensorTowerCumulativeScanBoostMax)
-    # 	if best then
-    # 		boost = boost + MulDivRound(const.SensorTowerScanBoostMax,
-    # 			Min(max_range - min_range, max_range - self:GetDist2D(best)),
-    # 			max_range - min_range)
-    # 	return boost
-    return MapSector(sx, sy).scan_boost([] if towers is None else towers)
 
 def parse_args(argv=None):
     parser = argparse.ArgumentParser(description=__doc__)
@@ -128,33 +139,27 @@ def parse_args(argv=None):
     log.debug(args)
     return args
 
+
 def main(argv: t.Optional[t.List[str]] = None):
     args = parse_args(argv)
     towers, mesh = inner_grid(MAP_SIZE, args.rank)
-    # Coords for TOWERS_GRID_RANK = 3
-    # [
-    #     (100, 100),
-    #     (100, 200),
-    #     (100, 300),
-    #     (200, 100),
-    #     (200, 200),
-    #     (200, 300),
-    #     (300, 100),
-    #     (300, 200),
-    #     (300, 300),
-    # ]
-    df = np.fromfunction(np.vectorize(scan_boost, otypes=[int], excluded=["towers"]), SECTOR_GRID, towers=towers)
+    df = MapSector.map_scan_boost(towers)
     _, ax = plt.subplots()
     sns.set_theme()
     # Good color maps (append "_r" to reverse):
-    # https://matplotlib.org/stable/users/explain/colors/colormaps.html
     # viridis, YlOrBr, Spectral, plasma, rainbow, jet
+    # https://matplotlib.org/stable/users/explain/colors/colormaps.html
     sns.heatmap(df, cmap="rainbow", annot=True, fmt='.0f', vmin=0, vmax=BOOST_MAX+NUM_BOOST_MAX, ax=ax, linewidths=1)
     plot = (m/s for m, s in zip(mesh, SECTOR_SIZE))  # Scale towers (x, y) to (sx, sy)
     plt.plot(*plot, marker='*', color="black", markersize=20, linestyle="")
+
+    # Maximize window on display. Works on Ubuntu with TkAgg backend
+    # See https://stackoverflow.com/q/12439588/624066
     mng = plt.get_current_fig_manager()
     mng.resize(*mng.window.maxsize())
+
     plt.show()
+
 
 if __name__ == "__main__":
     try:
